a91ab1aaf015546d813cb7cc8ab25f8eb1e689d8
dhruvpatel374 <dhruvpatel372004@gmail.com>
2024-01-26 11:48:55 +0530

    episode-03

--- App.js
+++ App.js
@@ -1,15 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I am H1 tag"),
-    React.createElement("h2", {}, "I am H2 tag"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I am H1 tag"),
-    React.createElement("h2", {}, "I am H2 tag"),
-  ]),
-]);
+// JSX => HTMLlike or XML like syntax not a  HTML
+
+const Title = () => <h1 className="heading">Namaste React From </h1>;
+const heading = () => (
+  <h1 className="heading1">Namaste React From javascript function</h1>
+);
+// React functional component
+const HeadingComponent = () => (
+  <div id="container">
+    <Title />
+    {heading}
+    {heading()}
+    <h1>This is React functional component🚀</h1>
+  </div>
+);
+// getting root element from element
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+// rendering root element
+root.render(<HeadingComponent />);
